7c556857a18c5508331d7ea0fb5e37927ec6f975
Will Dobbins <will.dobbins@gmail.com>
2017-10-13 13:58:35 -0700

    Renamed notes main executable, updated startup script.

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@
 go get github.com/codegangsta/gin
 go get -u github.com/golang/dep/cmd/dep
 /go/bin/dep ensure
-/go/bin/gin -a 8080 -d /go/src/github.com/willdobbins/notes/cmd/notes/ --path /go/src/github.com/willdobbins/notes run notes.go
+/go/bin/gin -a 8080 -d /go/src/github.com/willdobbins/notes/cmd/notesd/ --path /go/src/github.com/willdobbins/notes run main.go
